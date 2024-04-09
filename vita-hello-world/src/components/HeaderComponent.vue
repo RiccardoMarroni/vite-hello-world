eeb6c2a40b6e07fee8bc0151d92f6cb6cb81eed6
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-04-09 13:52:50 +0200

    header component

---
+++ vita-hello-world/src/components/HeaderComponent.vue
@@ -0,0 +1,24 @@
+<template>
+   <header>
+    {{ title }}
+   </header>
+</template>
+
+<script>
+    export default {
+        name:'HeaderComponent',
+        data(){
+            return{
+                title:'La mia prima App con Vite'
+            }
+        }
+    }
+</script>
+
+<style  scoped>
+header{
+    color: red;
+    text-align: center;
+    font-size: xx-large;
+}
+</style>
